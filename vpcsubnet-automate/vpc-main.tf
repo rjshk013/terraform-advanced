2baa382e98198676267c0caf9d09a5181e246d87
rjshk013 <rjshk013@gmail.com>
2021-05-20 17:14:56 +0530

    Update vpc-main.tf

--- vpcsubnet-automate/vpc-main.tf
+++ vpcsubnet-automate/vpc-main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count = "${length(var.subnets_cidr)}"
   vpc_id = "${aws_vpc.main.id}"
-  cidr_block = "${element(var.subnets_cidr,count.index)}"
+  cidr_block = "${element(var.publicsubnets_cidr,count.index)}"
   availability_zone = "${element(var.azs,count.index)}"
   map_public_ip_on_launch = true
   tags = {
